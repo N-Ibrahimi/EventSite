f3878993ce8d2e385f873ad4678f90f7d2a239b0
n-ibrahimi <nasim.hamdard21@gmail.com>
2019-02-13 19:55:17 +0100

    avant modification

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-    before_action :authenticate_user, only: [:new, :create, :edit]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
     def index
          @events=Event.all.order(start_date: :ASC)
@@ -15,8 +15,45 @@ class EventsController < ApplicationController
     end
   
     def new
+        @event = Event.new
     end
   
+    def subescribe
+        @event=Event.find(params[:id])
+
+        if @event.invited.include? current_user
+            flash[:error]= " vous êtes déja inscrit pour cet événement"
+            redirect_to @event
+        end 
+
+            @amount = @event.price 
+            
+            customer = Stripe::Customer.create(
+                :email => params[:stripeEmail],
+                :source  => params[:stripeToken]
+            )
+            
+            charge = Stripe::Charge.create(
+                :customer    => customer.id,
+                :amount      => @amount,
+                :description => 'Rails Stripe customer',
+                :currency    => 'eur'
+            )
+            
+            rescue Stripe::CardError => e
+            flash[:error] = e.message
+            redirect_to @event
+            end
+
+            @event.invited << current_user
+            flash[:success]="vous êtes bien inscrit pour cet evenement" 
+            redirect_to @event
+
+    end 
+
+
+
+
     def create 
         @event = Event.new(title: params[:title],
             description: params[:description],
@@ -26,22 +63,62 @@ class EventsController < ApplicationController
             location: params[:location],
             administrator_id: current_user.id )
 
-    if @event.save
-    redirect_to root_path, :notice => "Event créé !"
+        if @event.save
+        redirect_to root_path, :notice => "Event créé !"
 
-    end 
+        end 
+
+
+        # @event = Event.new(
+        #     start_date: params[:start_date], 
+        #     title: params[:title],
+        #     description: params[:description],
+        #     price: params[:price],
+        #     location: params[:location],
+        #     admin_id: current_user.id)
+        #     if @event.save
+        #       flash[:success] = "Event créé ! "
+        #       redirect_to event_path(@event.id)
+        #     else
+        #     end
+
+
     end
-  
 
   
-    def edit  
+    def edit 
+        @event = Event.find(params[:id]) 
     end
   
     def update 
+        @event = Event.find(params[:id])
+        if @event.update(event_params)
+        flash[:success] = "Ton événement a bien été modifié !"
+        redirect_to event_path(@event.id)
+        else
+        render :edit 
+        end
     end
   
     def destroy 
+        @event = Event.find(params[:id])
+        if @event.destroy
+        flash[:primary] = "Ton événement a bien été supprimé."
+        redirect_to root_path
+        return
+        else
+        end
         
     end
-end
+
+
+private
+
+    def event_params
+      params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location)
+    end
+  
+
+  
+
   
