2222233a09d2786a0815a793e83918eda04433c4
n-ibrahimi <nasim.hamdard21@gmail.com>
2019-02-13 08:51:39 +0100

    devise test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+        #  validates :first_name, presence: true
+        #  validates :last_name, presence: true
 
   has_many :attendances, foreign_key: 'invited_id' , dependent: :destroy
   has_many :events, foreign_key: 'admin_id',  dependent: :destroy
-  after_create :welcome_send
+
+
+        after_create :welcome_send
   
   
   def welcome_send
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+
+  get '/', to: 'home#index', as: "root"
+  get 'home_secret', to: 'home#secret', as: "home_secret"
+  
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
